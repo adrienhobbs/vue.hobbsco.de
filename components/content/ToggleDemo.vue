a017e8c0fd73ba609fc1f7766d84148ec7d57e2b
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-05-23 16:46:37 -0400

    update toggle demo

--- components/content/ToggleDemo.vue
+++ components/content/ToggleDemo.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
 import { useToggleEvents } from '@/composables/useToggleEvents'
+import { ref } from 'vue'
+const el = ref(null)
+const [onDarkMode, onLightMode, toggleDarkMode, isDark] = useToggleEvents()
 
-const [onTrue, onFalse, toggle, toggleValue] = useToggleEvents()
 
-onTrue(() => {
-  alert('you just toggled this to true!')
+
+onLightMode(() => {
+  document.documentElement.classList.remove('dark')
 })
 
-onFalse(() => {
-  alert('you just toggled this to false!')
+onDarkMode(() => {
+  document.documentElement.classList.add('dark')
 })
-
 </script>
 
 <template>
   <main class="min-h-[500px] flex items-center justify-center">
-    <button
+    <button ref="el"
       class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      @click="toggle">
-      Toggle {{ toggleValue ? 'off' : 'on' }}
+      @click="toggleDarkMode">
+      Toggle {{ isDark ? 'Light' : 'Dark' }} Mode
     </button>
   </main>
 </template>
